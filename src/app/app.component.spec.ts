19506b0f3ffe73be89f3e326276719a1d640e9d9
pouyababaie <pouyababie123@gmail.com>
2023-05-06 14:37:16 +0430

    changed test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('angular-github-actions app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('CI CD Test');
   });
 });
